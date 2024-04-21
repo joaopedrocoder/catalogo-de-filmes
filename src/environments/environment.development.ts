6b3e966018f2667166713650863068381a61f622
Joao Pedro <joaopedrolr.7@gmail.com>
2024-04-20 21:31:34 -0300

    configuração do environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  API_URL: "https://api.themoviedb.org/3/movie/",
+  API_KEY: "fa62259692f2ddfa9c80127ae8fc4a2a",
+  ACCOUNT_ID: "11221228"
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  API_URL: "",
+  API_KEY: ""
+};
